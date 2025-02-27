f4738e5b3f0453a3c8bca130ddb53dad3b2f032c
Anthony J Mangino <53193798+Aman10003@users.noreply.github.com>
2025-02-26 20:44:14 -0500

    Added ability to determine error type
    Added ability to set error type and error rate.

--- Server.py
+++ Server.py
@@ -1,8 +1,8 @@
 from socket import *
 import receive
 import send
-import struct
 from error_gen import error_gen
+import ast  # To safely convert string representation of a list back to a list
 
 
 class Server:
@@ -22,6 +22,18 @@ class Server:
             message, clientAddress = serverSocket.recvfrom(2048)
             message = message.decode()
 
+            if message == "PUSH" or message == "GET":
+                # Assuming you have a UDP socket set up as `serverSocket`
+                error_message, clientAddress = serverSocket.recvfrom(1024)  # Buffer size of 1024 bytes
+
+                # Decode and convert the message back to a list
+                decoded_message = error_message.decode()
+                received_list = ast.literal_eval(decoded_message)  # Safely parse the list
+
+                # Extract values
+                error_type = received_list[0]
+                error_rate = received_list[1]
+
             # Returns hello
             if message == 'HELLO':
                 serverSocket.sendto(message.encode(), clientAddress)
@@ -29,7 +41,7 @@ class Server:
             # Returns File to client
             elif message == 'GET':
                 s = send.send()
-                s.udp_send(serverSocket, clientAddress)
+                s.udp_send(serverSocket, clientAddress, error_type, error_rate)
 
                 # #Sending ACK back to client
                 # ack_packet = struct.pack("!H", 0)  # ACK with sequence number 0
@@ -39,7 +51,7 @@ class Server:
             # Receives file from client
             elif message == 'PUSH':
                 r = receive.receive()
-                r.udp_receive(serverSocket, True)
+                r.udp_receive(serverSocket, True, error_type, error_rate)
 
                 # # Sending another acknowledgment for PUSH
                 # ack_packet = struct.pack("!H", 1)  #ACK with sequence number 1
