30190cb40fdf60ee287b630354a8352d6412bda5
Nathan Li <nathanryoli@gmail.com>
2025-04-07 23:32:58 -0400

    Fix GUI protocol selection

--- gui.py
+++ gui.py
@@ -27,14 +27,13 @@ class gui:
         self.error_type_name = None
         self.response_textbox = None
         self.execute_button = None
-        self.transmit_type_name = None
         self.transmit_type = None
-
-        self.retrans_overhead_label = None
-        self.ack_eff_label = None
-        self.dup_ack_label = None
-        self.retrans_label = None
+        self.transmit_type_name = None
         self.progress_bar = None
+        self.retrans_label = None
+        self.dup_ack_label = None
+        self.ack_eff_label = None
+        self.retrans_overhead_label = None
 
     def run_server(self):
         # Creating a separate thread for running the server
@@ -61,33 +60,43 @@ class gui:
         message = 'GET'
         self.client_socket.sendto(message.encode(), (self.server_name, self.server_port))
 
-        message = str([self.error_type.value, self.error_rate.value])
-        self.client_socket.sendto(message.encode(), (self.server_name, self.server_port))
+        # Map the numeric selection to protocol string
+        transmit_map = {1: "sw", 2: "gbn", 3: "sr"}
+        protocol = transmit_map.get(self.transmit_type.value, "sw")
+
+        # Send error parameters along with the protocol choice
+        msg = str([self.error_type.value, self.error_rate.value, protocol])
+        self.client_socket.sendto(msg.encode(), (self.server_name, self.server_port))
 
         r = receive.receive()
-        r.udp_receive(self.client_socket, False, self.error_type.value, self.error_rate.value)
-        self.response_textbox.value += f'GET Response: Test\n'  # Append server response to text box
+        r.udp_receive_protocol(self.client_socket, False, self.error_type.value, self.error_rate.value, protocol)
+        self.response_textbox.value += f'GET Response: Image received.\n' # Append server response to text box
 
     def run_client_push(self):
         message = 'PUSH'
         self.client_socket.sendto(message.encode(), (self.server_name, self.server_port))
+
+        transmit_map = {1: "sw", 2: "gbn", 3: "sr"}
+        protocol = transmit_map.get(self.transmit_type.value, "sw")
+
+        msg = str([self.error_type.value, self.error_rate.value, protocol])
+        self.client_socket.sendto(msg.encode(), (self.server_name, self.server_port))
         s = send.send()
 
-        # Create a lock object
-        socket_lock = threading.Lock()
-
         def send_with_progress():
-            total_packets, retransmissions, duplicate_acks, ack_efficiency, retransmission_overhead = s.udp_send(
-                self.client_socket,
-                (self.server_name, self.server_port),
-                self.error_type.value,
-                self.error_rate.value,
-                update_ui_callback=[self.progress_bar,
-                                    self.retrans_label,
-                                    self.dup_ack_label,
-                                    self.ack_eff_label,
-                                    self.retrans_overhead_label]
-            )
+            total_packets, retransmissions, duplicate_acks, ack_efficiency, retransmission_overhead = \
+                s.udp_send_protocol(
+                    self.client_socket,
+                    (self.server_name, self.server_port),
+                    self.error_type.value,
+                    self.error_rate.value,
+                    protocol=protocol,
+                    update_ui_callback=[self.progress_bar,
+                                        self.retrans_label,
+                                        self.dup_ack_label,
+                                        self.ack_eff_label,
+                                        self.retrans_overhead_label]
+                )
             # ui.run(lambda: self.update_progress(1, retransmissions, duplicate_acks, ack_efficiency,retransmission_overhead))
 
             self.update_progress(1, retransmissions, duplicate_acks, ack_efficiency, retransmission_overhead)
@@ -130,10 +139,14 @@ class gui:
             self.run_client_push()
 
     def error_control(self, value):
+        # Make all error and protocol controls visible when needed
         self.error_rate_label.visible = value
         self.error_rate.visible = value
         self.error_rate_value.visible = value
-        self.error_type_name = value
+        self.error_type.visible = value
+        self.transmit_type.visible = value
+        self.error_type_name.visible = value
+        self.transmit_type_name.visible = value
 
     def create_ui(self):
         self.response_textbox = ui.textarea(label='Server Responses')
@@ -144,23 +157,27 @@ class gui:
             ui.button('Stop Server', on_click=self.stop_server)
 
         with ui.row():
-            get_button = ui.button('Get', on_click=self.get_control)
-            push_button = ui.button('Push', on_click=self.push_control)
+            ui.button('Get', on_click=self.get_control)
+            ui.button('Push', on_click=self.push_control)
 
         # Create a label and slider for selecting error rate (only visible when Get or Push)
         with ui.column():
             with ui.row():
                 self.error_type = ui.select([1, 2, 3, 4, 5], value=1)
                 self.error_type.visible = False
-                self.error_type_name = ui.select({1: 'No Error', 2: 'Ack Error', 3: 'Data Error', 4: 'Ack Lost', 5: 'Data Lost'}).bind_value(self.error_type, 'value')
+                self.error_type_name = ui.select(
+                    {1: 'No Error', 2: 'ACK Error', 3: 'Data Error', 4: 'ACK Lost', 5: 'Data Lost'}
+                ).bind_value(self.error_type, 'value')
             self.error_rate_label = ui.label('Select Error Rate')
             self.error_rate = ui.slider(min=0, max=1, step=0.01, value=0)
             self.error_rate_value = ui.label().bind_text_from(self.error_rate, 'value')
 
-        # Creates selection of RDT 3.0 vs GBN
+        # Protocol selection: 1 = RDT 3.0 (Stop-and-Wait), 2 = GBN, 3 = SR
         self.transmit_type = ui.select([1, 2, 3], value=1)
         self.transmit_type.visible = False
-        self.transmit_type_name = ui.select({1: 'RDT 3.0', 2: 'GBN', 3: 'SR'}).bind_value(self.transmit_type)
+        self.transmit_type_name = ui.select(
+            {1: 'RDT 3.0', 2: 'GBN', 3: 'SR'}
+        ).bind_value(self.transmit_type)
 
         self.execute_button = ui.button("Execute", on_click=self.execute)
 
